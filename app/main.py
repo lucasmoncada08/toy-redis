84a1628488450cedf7b240d7b3953fd830d9ce7b
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-08-04 13:06:40 -0400

    Added responding to multiple pings

--- app/main.py
+++ app/main.py
@@ -6,9 +6,10 @@ def main():
 
   server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
   client_connection, _ = server_socket.accept() # wait for client
-
-  client_connection.recv(1024)
-  client_connection.send(b"+PONG\r\n")
+  
+  while True:
+    client_connection.recv(1024)
+    client_connection.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
   main()
