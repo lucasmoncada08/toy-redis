855021f8bf3c5be2c319f9d6ebca4de64509c9ac
lucasmoncada08 <lucas.moncada08@gmail.com>
2022-08-09 09:46:29 -0400

    Added logging to application

--- app/client/client.py
+++ app/client/client.py
@@ -1,27 +1,29 @@
 import sys
 import os
+# import logging
+import warnings
+import time
 
+# ignoring import warning due to multiple same imports
+warnings.filterwarnings("ignore")
 
+# getting cwd to use when running client as a module
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 if os.path.dirname(SCRIPT_DIR) not in sys.path:
   sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-
-
 import socket
 from client.resp_encoder import RESPEncoder
-# from resp_encoder import RESPEncoder
-
 
 def run_client():
-  print(f'sys.path in fn: {sys.path}')
 
-  print("Client started")
+  print("<  Client Started\n")
 
   re = RESPEncoder()
 
   with socket.socket() as s:
     s.connect(("localhost", 6379))
+    print("<  Client Connected")
     # s.sendall(b"*3\r\n$3\r\nSET\r\n$4\r\nname\r\n$5\r\nlucas\r\n")
     # s.sendall(b"*3\r\n$3\r\nSET\r\n$5\r\nlucas\r\n$7\r\nmoncada\r\n")
     # s.sendall(b"*2\r\n$3\r\nGET\r\n$4\r\nname\r\n")
@@ -29,9 +31,10 @@ def run_client():
 
     # s.sendall(b"*4\r\n$3\r\nSET\r\n$5\r\nlucas\r\n$7\r\nmoncada\r\n$2\r\n10\r\n")
     while True:
-      inp = input("Enter Command\n")
+      inp = input(">  ")
 
       if inp == "exit":
+        print("<  Exiting")
         break
 
       if len(inp) == 0 or len(inp) >= 500:
@@ -39,15 +42,16 @@ def run_client():
         continue
 
       enc_inp = re.encode(inp)
-      print(enc_inp)
 
       s.sendall(enc_inp)
-      data = s.recv(1024)
-      
-      print(f'data recieved: {repr(data)}')
 
-    # return data
+      # time.sleep(1)
+      
+      data = s.recv(1024)
+
+      print(f'<  {repr(data)}\n')
+
+    return data
 
 if __name__ == "__main__":
   run_client()
-  pass
